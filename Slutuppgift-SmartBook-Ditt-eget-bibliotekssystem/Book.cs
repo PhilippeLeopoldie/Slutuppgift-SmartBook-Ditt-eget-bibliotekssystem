a372c5e670a2ddac851750733067627f7c7a76b0
Philippe Leopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-04-25 12:35:47 +0200

    feat(Book): Implement Book

--- Slutuppgift-SmartBook-Ditt-eget-bibliotekssystem/Book.cs
+++ Slutuppgift-SmartBook-Ditt-eget-bibliotekssystem/Book.cs
@@ -3,9 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Slutuppgift_SmartBook_Ditt_eget_bibliotekssystem.Enums;
 
 namespace Slutuppgift_SmartBook_Ditt_eget_bibliotekssystem;
 
-internal class Book
+public class Book
 {
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string ISBN { get; set; }
+    public GenreType Genre { get; set; }
+    public AvailabilityType Availability { get; set; }
+
 }
