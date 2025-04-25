0a94c28c7a8ec6d02c84ad1119d30851c872270c
Philippe Leopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-04-25 11:12:07 +0200

    Add project files.

---
+++ Slutuppgift-SmartBook-Ditt-eget-bibliotekssystem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Slutuppgift_SmartBook_Ditt_eget_bibliotekssystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
