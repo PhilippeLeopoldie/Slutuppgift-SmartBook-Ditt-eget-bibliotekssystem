40ee60ba1151c1e0712b48c6f7acd10a52711274
Philippe Leopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-04-25 11:41:37 +0200

    feat(setup): Add initial classes and folder structure

---
+++ Slutuppgift-SmartBook-Ditt-eget-bibliotekssystem/Enums/GenreType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slutuppgift_SmartBook_Ditt_eget_bibliotekssystem.Enums;
+
+internal class GenreType
+{
+}
