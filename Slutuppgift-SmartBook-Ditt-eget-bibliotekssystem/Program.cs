59ccf47d65ef530d97d0b899fd226123b0602932
Philippe Leopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-04-29 23:24:21 +0200

    feat(Program): implement Main method for application startup

--- Slutuppgift-SmartBook-Ditt-eget-bibliotekssystem/Program.cs
+++ Slutuppgift-SmartBook-Ditt-eget-bibliotekssystem/Program.cs
@@ -1,9 +1,27 @@
-﻿namespace Slutuppgift_SmartBook_Ditt_eget_bibliotekssystem;
+﻿using Slutuppgift_SmartBook_Ditt_eget_bibliotekssystem.Utils;
+using Slutuppgift_SmartBook_Ditt_eget_bibliotekssystem.UserInterface;
+
+namespace Slutuppgift_SmartBook_Ditt_eget_bibliotekssystem;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+
+        try
+        {
+            var stopProgram = 0;
+            var input = 0;
+            do
+            {
+                input = Util.intValidation(LibraryApp.StartPresentation());
+                LibraryApp.Run(input);
+            }
+            while (input != stopProgram);
+        }
+        catch (Exception e)
+        {
+            e.Message.Log();
+        }
     }
 }
