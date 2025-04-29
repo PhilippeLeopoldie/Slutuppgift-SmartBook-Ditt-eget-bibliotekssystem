3d5937e15ba0e11a52d3cab0443d75af8fae952a
Philippe Leopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-04-29 23:02:35 +0200

    feat(Book): Remove validation when setting fields

--- Slutuppgift-SmartBook-Ditt-eget-bibliotekssystem/Book.cs
+++ Slutuppgift-SmartBook-Ditt-eget-bibliotekssystem/Book.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Slutuppgift_SmartBook_Ditt_eget_bibliotekssystem.Enums;
+using Slutuppgift_SmartBook_Ditt_eget_bibliotekssystem.Isbn;
+using Slutuppgift_SmartBook_Ditt_eget_bibliotekssystem.Utils;
 
 namespace Slutuppgift_SmartBook_Ditt_eget_bibliotekssystem;
 
@@ -11,8 +13,40 @@ public class Book
 {
     public string Title { get; set; }
     public string Author { get; set; }
-    public string ISBN { get; set; }
+    public string Isbn { get; set; } = new IsbnGenerator().GenerateIsbn13();
+    
+
+
+    /*public string Title {
+        get => title; 
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                "Title cannot be empty.".ErrorMsg();
+            }
+            title = value;
+        } 
+    }*/
+    /*public string Author { 
+        get => author;
+        set 
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                "Author cannot be empty.".ErrorMsg();
+            }
+            author = value;
+        } 
+    }*/
+    
     public GenreType Genre { get; set; }
-    public AvailabilityType Availability { get; set; }
+    public AvailabilityType Availability { get; set; } = AvailabilityType.Available;
+
+
+    public override string ToString()
+    {
+        return $"ISBN:{Isbn}, Title:{Title}, Author:{Author}, Genre:{Genre}, Availability:{Availability}";
+    }
 
 }
