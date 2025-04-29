66a0ab0737e5f9876b5f2186dd05c714f8c08e96
Philippe Leopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-04-29 23:25:30 +0200

    Refactor(Ceed): Improve indentation in Ceed class

--- Slutuppgift-SmartBook-Ditt-eget-bibliotekssystem/Ceed/Ceed.cs
+++ Slutuppgift-SmartBook-Ditt-eget-bibliotekssystem/Ceed/Ceed.cs
@@ -13,15 +13,15 @@ internal static class Ceed
     public static void CeedBookList()
     {
         Library.Books.AddRange(
-new Book { Title = "Starfall", Author = "Alyssa Grey", Genre = GenreType.SciFi },
-new Book { Title = "The Last Note", Author = "Miles Carter", Genre = GenreType.Mystery },
-new Book { Title = "Ash and Flame", Author = "Lena Storm", Genre = GenreType.Fantasy },
-new Book { Title = "Silent Orbit", Author = "Noah Vance", Genre = GenreType.SciFi },
-new Book { Title = "Echoes of Guilt", Author = "Rachel Morn", Genre = GenreType.Mystery },
-new Book { Title = "The Iron Crown", Author = "Tessa Black", Genre = GenreType.Fantasy },
-new Book { Title = "Nova's Edge", Author = "Isaac Reed", Genre = GenreType.SciFi },
-new Book { Title = "The Vanishing Room", Author = "Emma Vale", Genre = GenreType.Mystery },
-new Book { Title = "Realmwalker", Author = "Kai Lennox", Genre = GenreType.Fantasy });
-
+            new Book { Title = "Starfall", Author = "Alyssa Grey", Genre = GenreType.SciFi },
+            new Book { Title = "The Last Note", Author = "Miles Carter", Genre = GenreType.Mystery },
+            new Book { Title = "Ash and Flame", Author = "Lena Storm", Genre = GenreType.Fantasy },
+            new Book { Title = "Silent Orbit", Author = "Noah Vance", Genre = GenreType.SciFi },
+            new Book { Title = "Echoes of Guilt", Author = "Rachel Morn", Genre = GenreType.Mystery },
+            new Book { Title = "The Iron Crown", Author = "Tessa Black", Genre = GenreType.Fantasy },
+            new Book { Title = "Nova's Edge", Author = "Isaac Reed", Genre = GenreType.SciFi },
+            new Book { Title = "The Vanishing Room", Author = "Emma Vale", Genre = GenreType.Mystery },
+            new Book { Title = "Realmwalker", Author = "Kai Lennox", Genre = GenreType.Fantasy }
+        );
     }
 }
